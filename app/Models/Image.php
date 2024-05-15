8c4f9ffa906b117d913f355b66b4c9930f7e5459
Misbahudin <misbahudin.id@gmail.com>
2024-05-15 15:26:02 +0700

    image for user

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -20,23 +20,10 @@ class Image extends Model
     protected $fillable = [
         "url",
         "user_id",
-        "teacher_id",
-        "student_id",
     ];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function teacher(): BelongsTo
-    {
-
-        return $this->belongsTo(Teacher::class);
-    }
-
-    public function student(): BelongsTo
-    {
-        return $this->belongsTo(Student::class);
     }
 }
